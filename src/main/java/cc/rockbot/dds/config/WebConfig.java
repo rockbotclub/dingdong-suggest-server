82d5543323d6ec68083032bf383f40a77fbd876b
rockbot <fengzhao.gfz@gmail.com>
2025-04-17 23:15:57 +0800

    feat: add CORS configuration to allow cross-origin requests

--- src/main/java/cc/rockbot/dds/config/WebConfig.java
+++ src/main/java/cc/rockbot/dds/config/WebConfig.java
@@ -3,6 +3,8 @@ package cc.rockbot.dds.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
@@ -10,5 +12,19 @@ public class WebConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                    .allowedOrigins("http://springboot-wu96-152263-4-1352937363.sh.run.tcloudbase.com", "https://springboot-wu96-152263-4-1352937363.sh.run.tcloudbase.com","http://localhost")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                    .allowedHeaders("*")
+                    .maxAge(3600);
+            }
+        };
     }
 } 
