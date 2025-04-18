d1cb31397c5f1238e5de2dffca87b4836ddbfdbf
rockbot <fengzhao.gfz@gmail.com>
2025-04-18 22:40:02 +0800

    fix: update database configuration and improve test coverage

--- src/main/java/cc/rockbot/dds/service/impl/SmsServiceImpl.java
+++ src/main/java/cc/rockbot/dds/service/impl/SmsServiceImpl.java
@@ -1,12 +1,7 @@
 package cc.rockbot.dds.service.impl;
 
 import cc.rockbot.dds.service.SmsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 
 /**
  * 短信验证码服务实现类
@@ -14,11 +9,7 @@ import java.util.concurrent.TimeUnit;
 @Service
 public class SmsServiceImpl implements SmsService {
 
-    private static final String SMS_CODE_PREFIX = "sms:code:";
     private static final int CODE_LENGTH = 6;
-    private static final int CODE_EXPIRE_MINUTES = 5;
-
-    
 
     @Override
     public boolean sendVerificationCode(String phoneNumber) {
@@ -33,6 +24,4 @@ public class SmsServiceImpl implements SmsService {
 
         return "111111".equals(code);
     }
-
-    
 } 
