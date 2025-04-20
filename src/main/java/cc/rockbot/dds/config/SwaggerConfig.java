2fae3e478ce2b2c06fcc4d0a985a1d7ccfc8be72
rockbot <fengzhao.gfz@gmail.com>
2025-04-20 11:22:33 +0800

    fix: resolve mixed content issue in Swagger UI by enforcing HTTPS and updating CORS configuration

--- src/main/java/cc/rockbot/dds/config/SwaggerConfig.java
+++ src/main/java/cc/rockbot/dds/config/SwaggerConfig.java
@@ -4,15 +4,20 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Swagger配置类
  * 用于配置API文档的生成
  */
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final DomainConfig domainConfig;
 
     @Bean
     public OpenAPI openAPI() {
@@ -27,6 +32,7 @@ public class SwaggerConfig {
                                 .url("https://github.com/rockops"))
                         .license(new License()
                                 .name("Apache 2.0")
-                                .url("https://www.apache.org/licenses/LICENSE-2.0")));
+                                .url("https://www.apache.org/licenses/LICENSE-2.0")))
+                .addServersItem(new Server().url("https://" + domainConfig.getBaseUrl()));
     }
 } 
